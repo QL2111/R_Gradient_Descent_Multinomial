3352318f5a0bf3da29a3b68ea74040b85e88ffaa
QL2111 <quentinlim384@gmail.com>
2024-11-01 00:54:04 +0100

    Documentation Logistic Regression

--- R/factor_analysis_mixed.R
+++ R/factor_analysis_mixed.R
@@ -4,6 +4,7 @@
 # A refaire ??? -> Ne prends pas en compte les variables qualitatives, génère juste des valeurs aléatoires
 # Il faudrait que factor_analysis_mixed prenne en compte les variables qualitatives de notre jeu de données
 # Pour le moment on va utiliser le one-hot encoding car je ne suis pas sûr que factor_analysis_mixed soit fonctionnel -Quentin
+
 factor_analysis_mixed <- function(data) {
   return(data.frame(matrix(rnorm(n = nrow(data) * 2), ncol = 2)))
 }
