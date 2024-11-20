46a3c6a6b5c3c8d395489cefca06dda3259b0b46
karamoko17 <awa.karamoko19@inphb.ci>
2024-11-20 22:58:56 +0100

    test et amélioration

--- Tests/test_code.R
+++ Tests/test_code.R
@@ -1,23 +1,33 @@
 # Charger les fichiers de fonctions
-
-source("R/LogisticRegressionMultinomial.R")
+source("D:/M2 SISE/Programmation R/Projet R/R_Gradient_Descent_Multinomial/R/LogisticRegressionMultinomial.R")
 # Charger le package R6
 library(R6)
 library(glmnet)
+library(nnet)
+library(caret)
 
-# Charger le jeu de données Iris
-data(iris)
+# Charger un fichier CSV dans R
+data <- read.csv("D:/M2 SISE/Programmation R/Projet R/dataset test/user_behavior_dataset.csv", header = TRUE, sep = ",")
+data
+colnames(data)
 
-# Préparer les données pour un modèle de régression logistique multinomiale
-# Convertir 'Species' en facteur pour avoir des classes
-iris$Species <- as.factor(iris$Species)
+# Extraire la variable cible 
+y <- data$Device.Model
+y
 
-# Séparer les prédicteurs (X) et la variable cible (y)
-X <- iris[, -5]  # Toutes les colonnes sauf Species
-y <- iris$Species
+# Encodage one-hot des variables catégorielles
+X <-data[, -c(1, 2)]
+X <- X[, sapply(X, is.numeric)]
+colnames(X)
+
+# Vérifier les valeurs m#anquantes
+any(is.na(X))       # Pour les valeurs manquantes
+
+X <- scale(X)
+y <- as.factor(y)
 
 #1. ################## Instancier le modèle avec les paramètres souhaités
-model <- LogisticRegressionMultinomial$new(learning_rate = 0.01, num_iterations = 1000)
+model <- LogisticRegressionMultinomial$new(learning_rate = 0.001, num_iterations = 1000)
 
 #2. ################# Ajuster le modèle sur le jeu de données
 model$fit(X, y)
@@ -36,22 +46,24 @@ print("Classes prédites pour chaque observation :")
 print(predictions)
 
 #6. ##################test de la fonction one_hot_encode
-unique_classes <- unique(iris$Species)
-one_hot_encoded_data <- model$one_hot_encode(iris$Species, unique_classes)
+unique_classes <- unique(y)
+one_hot_encoded_data <- model$one_hot_encode(y, unique_classes)
 print("Données après encodage one-hot :")
 print(head(one_hot_encoded_data))
-
 
 #7. ################## Appeler la méthode print() pour afficher les informations du modèle
 model$print()
 
 #8. ################## Instancier le modèle avec une valeur par défaut pour le seuil de sélection (exemple : 0.1)
-# Appliquer la sélection des variables sur le dataset iris
-selected_data <- model$var_select(iris[, -5], iris$Species) 
+# Appliquer la sélection des variables sur le dataset
+selected_data <- model$var_select(X, y)
 print("Données après sélection des variables :")
 print(head(selected_data))
 
+
 # Comparer les prédictions aux vraies classes
 accuracy <- mean(predictions == as.numeric(y))
-cat("Précision du modèle sur les données d'entraînement :", accuracy, "\n")
+cat("L'accuracy du modèle sur les données d'entraînement :", accuracy, "\n")
+
+
 
